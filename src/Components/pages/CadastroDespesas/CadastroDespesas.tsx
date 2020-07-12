56a54ac48d21d131866b18da7e5fedadf5956519
Emillyn Muniz <emillyn_muniz@hotmail.com>
2020-07-12 19:40:55 -0300

    Add files via upload

---
+++ src/Components/pages/CadastroDespesas/CadastroDespesas.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+
+const CadastroDespesas = (() => {
+    return(
+        <div>CadastroDespesas</div>
+    )
+})
+
+export default CadastroDespesas;
---
+++ src/Components/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+import './style.css';
+import fluxo from '../../../assets/fluxo.png'
+
+const Home = (() => {
+      return (
+          <div className="container-fluid">
+              
+          </div>
+    )});
+
+export default Home;
